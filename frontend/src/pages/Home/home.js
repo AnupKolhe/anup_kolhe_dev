586c8f6866aefd05242cf5bee1e6feb3c977937c
Anup Kolhe <69685239+AnupKolhe@users.noreply.github.com>
2026-09-14 18:52:36 +0530

    14 sep 2026

---
+++ frontend/src/pages/Home/home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Welcome to My App!</h1>
+            <p>This is the home page. Explore and enjoy your journey.</p>
+        </div>
+    );
+};
+
+export default Home;
